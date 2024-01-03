226e5ff268cc7c128992a7a9424dbd0b06b3c3b8
Rajit Deb <rajitdeb456@gmail.com>
2024-01-03 11:41:28 +0530

    Initial Commit

---
+++ app/src/main/java/com/rajit/workmanagerexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rajit.workmanagerexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
